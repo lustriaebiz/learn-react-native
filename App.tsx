b040a4d89e94ed92093e5c0685d1def8dd0c4fc5
Ebiz <lustriaebiz.cakap@gmail.com>
2019-11-29 18:16:14 +0700

    state

--- App.tsx
+++ App.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
+export default class App extends Component {
 
-  let picture = {
-    uri : 'https://statik.tempo.co/data/2018/12/14/id_804209/804209_720.jpg'
+  componentDidMount() {
+    console.log('componentDidMount');
+    
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to react native.. Ebiz</Text>
-      <Image source = {picture} style = {{width:193, height:110}}></Image>
-    </View>
-  );
+  state = {
+    picture : {
+      uri : 'https://statik.tempo.co/data/2018/12/14/id_804209/804209_720.jpg'
+    }
+  }
+
+  render () {
+    return(
+      <View style={styles.container}>
+        <Text>Welcome to react native.. Ebiz</Text>
+        <Image source = {this.state.picture} style = {{width:193, height:110}}></Image>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
